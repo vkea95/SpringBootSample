1f1b8cdbda79ac3f7a33e2b662b1e6808f9189ca
jianzhang <vkea95@gmail.com>
2018-07-06 14:52:33 -0700

    remove useless steps

--- src/main/java/com/vkea/sbs/config/ElasticSearchConfig.java
+++ src/main/java/com/vkea/sbs/config/ElasticSearchConfig.java
@@ -2,11 +2,9 @@ package com.vkea.sbs.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.vkea.sbs.repository")
-@PropertySource(ignoreResourceNotFound = false, value = {"classpath:application.properties"})
 public class ElasticSearchConfig {
 }
